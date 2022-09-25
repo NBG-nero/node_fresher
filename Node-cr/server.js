98adc5a46c85654fc4159bc574c341ea4d1b06a4
Aphra <oreofeojomo@gmail.com>
2022-09-25 21:44:53 +0100

    feat: package.json added amongst other things

--- Node-cr/server.js
+++ Node-cr/server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require('lodash');
+
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
@@ -16,7 +18,7 @@ const server = http.createServer((req, res) => {
       path += "about.html";
       res.statusCode = 200;
       break;
-    case "/about-me":
+    case "/about-us":
       res.statusCode = 301;
       res.setHeader("Location", "/about");
       res.end();
