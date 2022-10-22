d2c822c6709ccd31baa118fffe948766cdf7dead
Aphra <oreofeojomo@gmail.com>
2022-10-22 23:59:53 +0100

    feat: ejs

--- Node-cr/app.js
+++ Node-cr/app.js
@@ -3,11 +3,18 @@ const express = require("express");
 ///express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+// because my views folder is not int the immediate directory 
+//but in a specific folder, 
+//hence the need to specify
+app.set("views", "./Node-cr/views");
+
 //listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
