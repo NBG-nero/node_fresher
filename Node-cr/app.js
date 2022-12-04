2926a090e61e93813cf6131af9117835d0c8209d
Aphra <oreofeojomo@gmail.com>
2022-12-04 12:05:27 +0100

    feat: middleware

--- Node-cr/app.js
+++ Node-cr/app.js
@@ -11,7 +11,22 @@ app.set("view engine", "ejs");
 app.set("views", "./Node-cr/views");
 
 //listen for requests
+
 app.listen(3000);
+
+app.use((req, res, next) => {
+  console.log(" new request made:");
+  console.log("host: ", req.hostname);
+  console.log('"path: ', req.path);
+  console.log("method: ", req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(" in the next middleware:");
+
+  next();
+});
 
 app.get("/", (req, res) => {
   const blogs = [
@@ -28,7 +43,7 @@ app.get("/", (req, res) => {
       snippet: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
     },
   ];
-  res.render("index", { title: "Home" , blogs});
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
