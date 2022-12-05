458b93982e917fbe28a295186a71b7575d14bfd0
Aphra <oreofeojomo@gmail.com>
2022-12-05 23:14:21 +0100

    feat: middleware and static files with express and morgan

--- Node-cr/app.js
+++ Node-cr/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 ///express app
 const app = express();
@@ -14,19 +15,9 @@ app.set("views", "./Node-cr/views");
 
 app.listen(3000);
 
-app.use((req, res, next) => {
-  console.log(" new request made:");
-  console.log("host: ", req.hostname);
-  console.log('"path: ', req.path);
-  console.log("method: ", req.method);
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log(" in the next middleware:");
-
-  next();
-});
+// middleware and static files
+app.use(express.static("Node-cr/public"));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
