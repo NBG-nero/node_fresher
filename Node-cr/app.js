7a744baec9ff287f975f3c16ec77c25e6857cc97
Aphra <oreofeojomo@gmail.com>
2022-12-06 23:45:17 +0100

    feat: connected to mongodb with mongoose

--- Node-cr/app.js
+++ Node-cr/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 ///express app
 const app = express();
+//mongoose fucker
+mongoose.set("strictQuery", true);
 
+//connect to mongodb
+const dbURI =
+  "mongodb+srv://NetNigga:test1234@node.1q6axec.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log("connected to db"))
+  .catch((err) => console.log(err));
 // register view engine
 app.set("view engine", "ejs");
 // because my views folder is not int the immediate directory
