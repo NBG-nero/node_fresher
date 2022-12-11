56fa3657beca6907ddf240f884fbdf79c6673bec
Aphra <oreofeojomo@gmail.com>
2022-12-11 23:18:47 +0100

    feat: minor untested post req changes

--- Node-cr/app.js
+++ Node-cr/app.js
@@ -27,6 +27,7 @@ app.set("views", "./Node-cr/views");
 
 // middleware and static files
 app.use(express.static("Node-cr/public"));
+app.use(express.urlencoded({extended: true}));
 app.use(morgan("dev"));
 
 //routes
@@ -51,6 +52,15 @@ app.get("/blogs", (req, res) => {
       console.log(err);
     });
 });
+
+app.post("/blogs",(req , res)=>{ 
+const  blog = new Blog(req.body);
+  blog.save().then((result)=> { 
+res.redirect("/blogs");
+  }).catch((err) => {console.log(err);
+  });
+});
+
 
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new Blog" });
