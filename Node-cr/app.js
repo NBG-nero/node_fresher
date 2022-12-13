17a8e8db52707bd8fb0ba4ae4fccd2a06cda296f
Aphra <oreofeojomo@gmail.com>
2022-12-13 12:23:02 +0100

    feat: post request and get request with id

--- Node-cr/app.js
+++ Node-cr/app.js
@@ -66,6 +66,21 @@ app.post("/blogs", (req, res) => {
     });
 });
 
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", {
+        blog: result,
+        title: "Blog Details",
+      });
+    })
+    .then((err) => {
+      console.log(err);
+    });
+});
+
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new Blog" });
 });
