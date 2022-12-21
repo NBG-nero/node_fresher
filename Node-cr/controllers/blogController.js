69051803fbfa227e5ce22b5f2b886a62bac47e83
Aphra <oreofeojomo@gmail.com>
2022-12-21 12:04:31 +0100

    feat: refiling

--- Node-cr/controllers/blogController.js
+++ Node-cr/controllers/blogController.js
@@ -6,7 +6,7 @@ const blog_index = (req, res) => {
   Blog.find()
     .sort({ createdAt: -1 })
     .then((result) => {
-      res.render("index", {
+      res.render("blogs/index", {
         title: " All Blog",
         blogs: result,
       });
@@ -21,7 +21,7 @@ const blog_details = (req, res) => {
   console.log(id);
   Blog.findById(id)
     .then((result) => {
-      res.render("details", {
+      res.render("blogs/details", {
         blog: result,
         title: "Blog Details",
       });
@@ -32,7 +32,7 @@ const blog_details = (req, res) => {
 };
 
 const blog_create_get = (req, res) => {
-  res.render("create", { title: "Create a new Blog" });
+  res.render("blogs/create", { title: "Create a new Blog" });
 };
 
 const blog_create_post = (req, res) => {
@@ -47,24 +47,21 @@ const blog_create_post = (req, res) => {
     });
 };
 
-
-const blog_delete =(req, res) => { 
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-      .then((result) => {
-        res.json({ redirect: "/blogs" });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+const blog_delete = (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
-
-
 
 module.exports = {
   blog_index,
   blog_details,
   blog_create_get,
   blog_create_post,
-  blog_delete
+  blog_delete,
 };
